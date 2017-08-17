48226eaf10900b6f08a2c0ed73715ddae8015816
Jave.Cai <kasonpt@163.com>
2017-08-17 23:37:10 +0800

    [ex8-7] add solution for ex8-7 at first level

--- solutions/ch8/ex8-7/ex8-7.go
+++ solutions/ch8/ex8-7/ex8-7.go
@@ -1,20 +1,28 @@
 //ex8-7
 //base on crawl3 and add a check for domain and save the page to local
-//date:20170817
+//start date:20170817
 
+/* depart to 3 steps:
+1. fetch all the html from the url and save in local path
+2. change the url inside to the local path
+3. fetch the picture,css,js also
+*/
 package main
 
 import (
 	"fmt"
+	"golang.org/x/net/html"
+	"io"
 	"log"
+	"net/http"
 	"os"
-
-	"gopl.io/ch5/links"
+	"path"
+	"strings"
 )
 
 func crawl(url string) []string {
 	fmt.Println(url)
-	list, err := links.Extract(url)
+	list, err := Extract(url)
 	if err != nil {
 		log.Print(err)
 	}
@@ -34,6 +42,10 @@ func main() {
 		go func() {
 			for link := range unseenLinks {
 				//TODO: save the page
+				_, _, err := fetch(link)
+				if err != nil {
+					fmt.Println("fetch err: %v", err)
+				}
 				foundLinks := crawl(link)
 				go func() { worklist <- foundLinks }()
 			}
@@ -45,7 +57,7 @@ func main() {
 	seen := make(map[string]bool)
 	for list := range worklist {
 		for _, link := range list {
-			if /*TODO: check the link if it start from domain && */ !seen[link] {
+			if strings.HasPrefix(link, os.Args[1]) && !seen[link] {
 				seen[link] = true
 				unseenLinks <- link
 			}
@@ -54,3 +66,93 @@ func main() {
 }
 
 //!-
+
+//come from //ch5/fetch
+
+//!+
+// Fetch downloads the URL and returns the
+// name and length of the local file.
+func fetch(url string) (filename string, n int64, err error) {
+	fmt.Println("fetch url: " + url)
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+	fmt.Println("resp url path: " + resp.Request.URL.Path)
+	local := path.Base(resp.Request.URL.Path)
+	fmt.Println("local path: " + local)
+	if local == "/" {
+		local = "index.html"
+	}
+	err = os.MkdirAll(resp.Request.URL.Path[1:], os.ModeDir)
+	if err != nil {
+		return "", 0, err
+	}
+	f, err := os.Create(resp.Request.URL.Path[1:] + "index.html")
+	if err != nil {
+		return "", 0, err
+	}
+	n, err = io.Copy(f, resp.Body)
+	// Close file, but prefer error from Copy, if any.
+	if closeErr := f.Close(); err == nil {
+		err = closeErr
+	}
+	return local, n, err
+}
+
+//!-
+
+//come from ch5/link
+
+// Extract makes an HTTP GET request to the specified URL, parses
+// the response as HTML, and returns the links in the HTML document.
+func Extract(url string) ([]string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		resp.Body.Close()
+		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
+	}
+
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
+	}
+
+	var links []string
+	visitNode := func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key != "href" {
+					continue
+				}
+				link, err := resp.Request.URL.Parse(a.Val)
+				if err != nil {
+					continue // ignore bad URLs
+				}
+				links = append(links, link.String())
+			}
+		}
+	}
+	forEachNode(doc, visitNode, nil)
+	return links, nil
+}
+
+//!-Extract
+
+// Copied from gopl.io/ch5/outline2.
+func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
+	if pre != nil {
+		pre(n)
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+	if post != nil {
+		post(n)
+	}
+}
