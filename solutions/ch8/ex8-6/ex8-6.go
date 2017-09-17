693ab7838b1dbcb5cdb1294e7d0d341fe0ec7dae
Jave.Cai <kasonpt@163.com>
2017-09-17 23:18:19 +0800

    [ex8-6] A good version from torbiak.

--- solutions/ch8/ex8-6/ex8-6.go
+++ solutions/ch8/ex8-6/ex8-6.go
@@ -1,116 +1,58 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-
-// See page 243.
-
-// Crawl3 crawls web links starting with the command-line arguments.
+// ex8.6 is a depth-limited web crawler.
 //
-// This version uses bounded parallelism.
-// For simplicity, it does not address the termination problem.
-//
+// Use a WaitGroup to determine when the work is done, the `tokens` chan as a
+// semaphore to limit concurrent requests, and a mutex around the `seen` map to
+// avoid concurrent reads and writes.
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
-	"net/http"
-	"os"
+	"sync"
 
-	"golang.org/x/net/html"
+	"gopl.io/ch5/links"
 )
 
-func crawl(url string) []string {
-	fmt.Println(url)
-	list, err := Extract(url)
+// tokens is a counting semaphore used to
+// enforce a limit of 20 concurrent requests.
+var tokens = make(chan struct{}, 20)
+var maxDepth int
+var seen = make(map[string]bool)
+var seenLock = sync.Mutex{}
+
+func crawl(url string, depth int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(depth, url)
+	if depth >= maxDepth {
+		return
+	}
+	tokens <- struct{}{} // acquire a token
+	list, err := links.Extract(url)
+	<-tokens // release the token
 	if err != nil {
 		log.Print(err)
 	}
-	return list
+	for _, link := range list {
+		seenLock.Lock()
+		if seen[link] {
+			seenLock.Unlock()
+			continue
+		}
+		seen[link] = true
+		seenLock.Unlock()
+		wg.Add(1)
+		go crawl(link, depth+1, wg)
+	}
 }
 
-//!+
 func main() {
-	worklist := make(chan []string)  // lists of URLs, may have duplicates
-	unseenLinks := make(chan string) // de-duplicated URLs
-
-	// Add command-line arguments to worklist.
-	go func() { worklist <- os.Args[1:] }()
-
-	// Create 20 crawler goroutines to fetch each unseen link.
-	for i := 0; i < 20; i++ {
-		go func() {
-			for link := range unseenLinks {
-				foundLinks := crawl(link)
-				go func() { worklist <- foundLinks }()
-			}
-		}()
+	flag.IntVar(&maxDepth, "d", 3, "max crawl depth")
+	flag.Parse()
+	wg := &sync.WaitGroup{}
+	for _, link := range flag.Args() {
+		wg.Add(1)
+		go crawl(link, 0, wg)
 	}
-
-	// The main goroutine de-duplicates worklist items
-	// and sends the unseen ones to the crawlers.
-	seen := make(map[string]bool)
-	for list := range worklist {
-		for _, link := range list {
-			if !seen[link] {
-				seen[link] = true
-				unseenLinks <- link
-			}
-		}
-	}
-}
-
-//!-
-
-// copy from "gopl.io/ch5/links"
-
-// Extract makes an HTTP GET request to the specified URL, parses
-// the response as HTML, and returns the links in the HTML document.
-func Extract(url string) ([]string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	if resp.StatusCode != http.StatusOK {
-		resp.Body.Close()
-		return nil, fmt.Errorf("getting %s: %s", url, resp.Status)
-	}
-
-	doc, err := html.Parse(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		return nil, fmt.Errorf("parsing %s as HTML: %v", url, err)
-	}
-
-	var links []string
-	visitNode := func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, a := range n.Attr {
-				if a.Key != "href" {
-					continue
-				}
-				link, err := resp.Request.URL.Parse(a.Val)
-				if err != nil {
-					continue // ignore bad URLs
-				}
-				links = append(links, link.String())
-			}
-		}
-	}
-	forEachNode(doc, visitNode, nil)
-	return links, nil
-}
-
-//!-Extract
-
-// Copied from gopl.io/ch5/outline2.
-func forEachNode(n *html.Node, pre, post func(n *html.Node)) {
-	if pre != nil {
-		pre(n)
-	}
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		forEachNode(c, pre, post)
-	}
-	if post != nil {
-		post(n)
-	}
+	wg.Wait()
 }
